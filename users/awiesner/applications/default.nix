19a931d98590b1cce760ae2605d74525592fbfa3
prolo2408 <artur.w2408@gmail.com>
2026-06-06 12:26:58 +0200

    Cleand up

--- users/awiesner/applications/default.nix
+++ users/awiesner/applications/default.nix
@@ -9,4 +9,14 @@
   imports = [
     ./browser/default.nix
   ];
+
+    home.packages = with pkgs; [
+    firefox
+    htop
+    fastfetch
+    kdePackages.kate
+    vscode
+    vim
+    git
+  ];
 }
--- users/awiesner/default.nix
+++ users/awiesner/default.nix
@@ -9,18 +9,7 @@
   home.username = userName;
   home.homeDirectory = "/home/${userName}";
 
-  # Andere normale Apps, die keine eigene Config brauchen
-  home.packages = with pkgs; [
-    firefox
-    htop
-    fastfetch
-    kdePackages.kate
-    vscode
-    vim
-    git
-  ];
-
-   # Bash (oder zsh) konfigurieren und Aliase anlegen
+  # Bash (oder zsh) konfigurieren und Aliase anlegen
   programs.bash = {
     enable = true;
     shellAliases = {
